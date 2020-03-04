9ddf8ad18d8f5355b7ee2b6b0b9855c7379515bd
pavlinpetrov <pavlinpetrovdev@gmail.com>
2020-03-04 09:36:58 +0200

    Add contact model

---
+++ models/ContactModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ContactSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Contact', ContactSchema);
