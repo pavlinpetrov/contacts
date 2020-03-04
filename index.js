f87181a90bc61d2109a80134ef312dc8fce15a65
pavlinpetrov <pavlinpetrovdev@gmail.com>
2020-03-04 21:03:03 +0200

    Add semicolon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { database, corsOptions } = require('./config');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
 
 // Database
